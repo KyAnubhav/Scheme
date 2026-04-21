951592a921f4dcdbd0469dc62d94eb7e679ab756
Anubhav <kyanubhav@gmail.com>
2026-04-21 21:33:59 +0530

    configurr

--- db.js
+++ db.js
@@ -1,22 +1,21 @@
 const sql = require("mssql");
 
 const config = {
-    user: "sa",
-    password: "manmith123",
-    server: "localhost",
-    port: 1433,
-    database: "governmentScheme",
-    options: {
-        trustServerCertificate: true
-    }
+  user: "sa",
+  password: "dbms sql 1990",
+  server: "localhost\\SQLEXPRESS",   
+  database: "governmentScheme",
+  options: {
+    trustServerCertificate: true
+  }
 };
 
 const pool = new sql.ConnectionPool(config)
-    .connect()
-    .then(pool => {
-        console.log("Connected to SQL Server");
-        return pool;
-    })
-    .catch(err => console.log("DB ERROR:", err));
+  .connect()
+  .then(pool => {
+    console.log("Connected to SQL Server");
+    return pool;
+  })
+  .catch(err => console.log("DB ERROR:", err));
 
 module.exports = { pool };
